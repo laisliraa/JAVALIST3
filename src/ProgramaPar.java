9f5bd3a58c468be61f7b64a00813d8633c4a346f
Lais Lira <laislirap04@hotmail.com>
2022-11-21 10:17:32 -0300

    first commit

--- src/ProgramaPar.java
+++ src/ProgramaPar.java
@@ -10,7 +10,20 @@ public class ProgramaPar {
 
         //input para o usuário inserir os valores 
         //o laço de repetição for vai simplificar o looping de perguntas para o usuário
-        
+        for (int i = 0; i < Numbers.length; i++) {
+            System.out.println("Digite um número aleatório: ");
+            Numbers[i] = input.nextInt();
+        }
 
+        System.out.println("Os números pares são: ");
+
+        //laço de repetição
+        for (int result = 0; result < Numbers.length; result++){
+            if(Numbers[result] % 2 == 0){
+                System.out.println(Numbers[result]);
+            }
+        }
+
+        input.close();
     }
 }
